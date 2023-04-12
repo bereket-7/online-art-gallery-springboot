384c549b1ebb8bc9055f299843f7ac9d00d2cc31
bereket-7 <bekhonelign@gmail.com>
2023-04-12 11:36:26 -0700

    organization controller

--- src/main/java/com/project/oag/service/OrganizationService.java
+++ src/main/java/com/project/oag/service/OrganizationService.java
@@ -1,23 +1,15 @@
 package com.project.oag.service;
 
 import java.util.List;
+import java.util.Optional;
 
-import com.google.common.base.Optional;
-import com.project.oag.controller.dto.OrganizationDto;
 import com.project.oag.entity.Organization;
-import com.project.oag.exceptions.UserAlreadyExistException;
 
 public interface OrganizationService {
 
-	// Organization update(OrganizationDto organizationDto) throws
-	// UserNotFoundException;
 	void updatePassword(Organization organization, String password);
 
-	// void updateResetPasswordToken(String token, String email) throws
-	// UserNotFoundException;
 	Organization getByResetPasswordToken(String token);
-
-	Organization save(OrganizationDto organizationDto) throws UserAlreadyExistException;
 
 	public void addOrganization(Organization org);
 
