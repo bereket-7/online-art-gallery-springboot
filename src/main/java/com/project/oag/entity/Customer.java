de2eedca977b0c30d89d70f4c3019f2a19f5fcfd
bereket honelign <bekhonelign@gmail.com>
2023-04-15 22:03:38 +0300

    customer role update

--- src/main/java/com/project/oag/entity/Customer.java
+++ src/main/java/com/project/oag/entity/Customer.java
@@ -1,10 +1,17 @@
 package com.project.oag.entity;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 @Table(name="Customer")
@@ -15,21 +22,29 @@ public class Customer {
     private String firstname;
     private String lastname;
     private String email;
-   private String phone;
-   private String address;
-   private Integer age;
-   private String sex;
+    private String phone;
+    private String address;
+    private Integer age;
+    private String sex;
     private String password;
-   private boolean enabled;
+    private boolean enabled;
     private String token;
-
-    
-    public Customer() {
-        super();
+    private boolean isUsing2FA;
+     
+    public boolean isUsing2FA() {
+        return isUsing2FA;
     }
 
+    public void setUsing2FA(boolean isUsing2FA) {
+        this.isUsing2FA = isUsing2FA;
+    }
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "customer_roles",joinColumns = @JoinColumn(name = "customer_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set roles;
+
     public Customer(String firstname, String lastname, String email, String phone, String address, Integer age,
-            String sex, String password, boolean enabled, String token) {
+            String sex, String password, boolean enabled, String token, boolean isUsing2FA, Set roles) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
@@ -40,13 +55,29 @@ public class Customer {
         this.password = password;
         this.enabled = enabled;
         this.token = token;
+        this.isUsing2FA = isUsing2FA;
+        this.roles = roles;
     }
+
+    public Customer() {
+        super();
+    }
+
     
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
+    }
+
+    
+    public Set getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set roles) {
+        this.roles = roles;
     }
     public String getFirstname() {
         return firstname;
