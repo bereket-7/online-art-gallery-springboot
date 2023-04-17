f03a986b9216fa3862f6c36973ee9fcf5ebde06d
bereket honelign <bekhonelign@gmail.com>
2023-04-17 13:09:22 +0300

    authentication token

--- src/main/java/com/project/oag/repository/CartRepository.java
+++ src/main/java/com/project/oag/repository/CartRepository.java
@@ -6,13 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.oag.entity.Cart;
 import com.project.oag.entity.User;
-
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long>{
 
-	List<Cart> findAllByUserIdOrderByCreatedDateDesc(Integer userId);
-	List<Cart> deleteByUser(User user);
 	List<Cart> findAllByUserOrderByCreatedDateDesc(User user);
-	void deleteById(int id);
-	//Cart getOne(Integer id);
+
+    List<Cart> deleteByUser(User user);
 
 }
