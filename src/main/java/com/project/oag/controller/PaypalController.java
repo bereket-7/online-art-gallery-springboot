4666853bf7a0c6ad5ab7c062c94e31af43061125
bereket honelign <bekhonelign@gmail.com>
2023-04-26 08:03:52 +0300

    thymeleaf

--- src/main/java/com/project/oag/controller/PaypalController.java
+++ src/main/java/com/project/oag/controller/PaypalController.java
@@ -1,7 +1,6 @@
 package com.project.oag.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
