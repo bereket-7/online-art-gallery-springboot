c1c8873b96c368790cc3e7166002432499344ade
bereket honelign <bekhonelign@gmail.com>
2023-04-26 19:30:02 +0300

    bid controller

--- src/main/java/com/project/oag/service/BidService.java
+++ src/main/java/com/project/oag/service/BidService.java
@@ -1,11 +1,17 @@
 package com.project.oag.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.project.oag.entity.Bid;
+import com.project.oag.entity.BidArt;
 import com.project.oag.repository.BidRepository;
 
 @Service
@@ -46,4 +52,31 @@ public class BidService {
     public void deleteBid(Long id) {
         bidRepository.deleteById(id);
     }
+
+ 
+    @Scheduled(fixedDelay = 10000)
+    public void checkBidEndTimes() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Bid> bids = bidRepository.findAll();
+        for (Bid bid : bids) {
+            if (bid.getBidEndTime().isBefore(now) && !bid.isBiddingClosed()) {
+                bid.setBiddingClosed(true);
+                // Determine the winner of the auction
+                Bid winningBid = // get the winning bid
+                // Notify the bidders of the result
+                // Send a message to the WebSocket clients to update their UIs
+            }
+        }
+    }
+
+    public BigDecimal getCurrentHighestBidAmount(BidArt bidArt) {
+        List<Bid> bids = bidArt.getBids();
+        if (bids.isEmpty()) {
+            return bidArt.getInitialAmount();
+        } else {
+            Bid highestBid = Collections.max(bids, Comparator.comparing(Bid::getAmount));
+            return highestBid.getAmount();
+        }
+    }
 }
+
