46d0a0e8da7ed9c106a6fbe1827f8aa947981f5c
bereket honelign <bekhonelign@gmail.com>
2023-04-26 21:37:41 +0300

    update user

--- src/main/java/com/project/oag/entity/User.java
+++ src/main/java/com/project/oag/entity/User.java
@@ -1,6 +1,5 @@
 package com.project.oag.entity;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -14,7 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToMany;  
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
