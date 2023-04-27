f163890d5508f954c69ae8bad25397308f837949
bereket honelign <bekhonelign@gmail.com>
2023-04-27 08:55:05 +0300

    manage handler

--- src/main/java/com/project/oag/configuration/WebSocketConfig.java
+++ src/main/java/com/project/oag/configuration/WebSocketConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import com.project.oag.service.BidHandler;
+
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
