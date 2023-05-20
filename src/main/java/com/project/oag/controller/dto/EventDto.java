5fea22ed7c899d34c37d69714691d68edae64876
bereket honelign <bekhonelign@gmail.com>
2023-05-20 21:38:33 +0300

    event dto

--- src/main/java/com/project/oag/controller/dto/EventDto.java
+++ src/main/java/com/project/oag/controller/dto/EventDto.java
@@ -2,16 +2,7 @@ package com.project.oag.controller.dto;
 
 import java.time.LocalDate;
 
-import org.springframework.core.io.Resource;
-
-import com.project.oag.entity.Event;
-
-public class EventDto {
-
-    //private String eventPhoto; // Add a new field to hold the URL of the event photo
-    //private String imageData;
-    
-    
+public class EventDto {  
         private String eventName;
         private String eventDescription;
         private LocalDate eventDate;
@@ -21,37 +12,15 @@ public class EventDto {
         private String status;    
         private Long id;
         private byte[] image;
-    	
-  
-    // Constructors
- /*   public EventDto() {
-    	super();
-    }
-    
-    public EventDto(Event event) {
-        this.id = event.getId();
-        this.eventName = event.getEventName();
-        this.eventDescription = event.getEventDescription();
-        this.eventDate = event.getEventDate();
-        this.location = event.getLocation();
-        this.capacity = event.getCapacity();
-        this.ticketPrice = event.getTicketPrice();
-        this.eventPhoto = event.getEventPhoto(); // Set the photo URL to the event's photo path
-    }*/
-    
-        
-        
-    public byte[] getImage() {
+    	    
+      
+   public byte[] getImage() {
 			return image;
 		}
 
 		public void setImage(byte[] image) {
 			this.image = image;
 		}
-
-	public EventDto(Event event, Resource file) {
-		// TODO Auto-generated constructor stub
-	}
 
 	public EventDto() {
 		// TODO Auto-generated constructor stub
