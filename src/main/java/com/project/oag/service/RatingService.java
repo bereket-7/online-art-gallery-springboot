606cac5876b06589aaacbe59a15273aff381279b
bereket-7 <bekhonelign@gmail.com>
2023-05-25 09:44:43 -0700

    rate serv

--- src/main/java/com/project/oag/service/RatingService.java
+++ src/main/java/com/project/oag/service/RatingService.java
@@ -23,42 +23,6 @@ public class RatingService {
 	
 	@Autowired
 	private UserRepository userRepository;
-
-	/*
-	 public void addRating(User user, Artwork artwork, int rating) throws RatingAlreadyExistsException {
-	        String userIdAndArtworkId = user.getId() + ":" + artwork.getId();
-
-	        Optional<Rating> existingRating = ratingRepository.findByUserIdAndArtworkId(userIdAndArtworkId);
-	        if (existingRating.isPresent()) {
-	            throw new RatingAlreadyExistsException("User " + user.getUsername() + " has already rated artwork " + artwork.getArtworkName());
-	        }
-
-	        Rating newRating = new Rating(user, artwork, rating);
-	        ratingRepository.save(newRating);
-	    }*/
-	 
-//	 public void addRating(Long userId, Long artworkId, int rating) throws ArtworkNotFoundException, UserNotFoundException, RatingAlreadyExistsException {
-//		    Artwork artwork = artworkRepository.findById(artworkId).orElse(null);
-//		    if (artwork == null) {
-//		        throw new ArtworkNotFoundException("Artwork not found with ID: " + artworkId);
-//		    }
-//
-//		    User user = userRepository.findById(userId).orElse(null);
-//		    if (user == null) {
-//		        throw new UserNotFoundException("User not found with ID: " + userId);
-//		    }
-//
-//		    Rating existingRating = ratingRepository.findByUserAndArtwork(user, artwork);
-//		    if (existingRating != null) {
-//		        throw new RatingAlreadyExistsException("User " + userId + " has already rated artwork " + artworkId);
-//		    }
-//
-//		    Rating newRating = new Rating();
-//		    newRating.setUser(user);
-//		    newRating.setArtwork(artwork);
-//		    newRating.setRating(rating);
-//		    ratingRepository.save(newRating);
-//		}
 	
 	public void addRating(Long userId, Long artworkId, int rating) throws ArtworkNotFoundException, UserNotFoundException, RatingAlreadyExistsException {
 	    Artwork artwork = artworkRepository.findById(artworkId).orElse(null);
@@ -83,23 +47,6 @@ public class RatingService {
 	    newRating.setRating(rating);
 	    ratingRepository.save(newRating);
 	}
-
-
-//	public void addRating(Long userId, Long artworkId, int rating) throws ArtworkNotFoundException, UserNotFoundException {
-//	    Artwork artwork = artworkRepository.findById(artworkId).orElse(null);
-//	    if (artwork == null) {
-//	        throw new ArtworkNotFoundException("Artwork not found with ID: " + artworkId);
-//	    }
-//	    User user = userRepository.findById(userId).orElse(null);
-//	    if (user == null) {
-//	        throw new UserNotFoundException("User not found with ID: " + userId);
-//	    }
-//	    Rating newRating = new Rating();
-//	    newRating.setUser(user);
-//	    newRating.setArtwork(artwork);
-//	    newRating.setRating(rating);
-//	    ratingRepository.save(newRating);
-//	}
 	
 	public double getAverageRatingForArtwork(Long artworkId) throws ArtworkNotFoundException {
 	    Artwork artwork = artworkRepository.findById(artworkId).orElse(null);
@@ -108,22 +55,5 @@ public class RatingService {
 	    }
 	    return artwork.getAverageRating();
 	}
-
-		
-//	public double getAverageRatingForArtwork(Long artworkId) throws ArtworkNotFoundException {
-//	    Artwork artwork = artworkRepository.findById(artworkId).orElse(null);
-//	    if (artwork == null) {
-//	        throw new ArtworkNotFoundException("Artwork not found with ID: " + artworkId);
-//	    }
-//	    List<Rating> ratings = ratingRepository.findByArtwork(artwork);
-//	    if (ratings.isEmpty()) {
-//	        return 0.0;
-//	    }
-//	    double sum = 0;
-//	    for (Rating rating : ratings) {
-//	        sum += rating.getRating();
-//	    }
-//	    return sum / ratings.size();
-//	}
 
 }
