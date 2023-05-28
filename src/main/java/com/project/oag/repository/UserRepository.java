fc5f97619db146f23db0e518770b6a7ed80d2f87
bereket-7 <bekhonelign@gmail.com>
2023-05-28 08:20:24 -0700

    user repo

--- src/main/java/com/project/oag/repository/UserRepository.java
+++ src/main/java/com/project/oag/repository/UserRepository.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.project.oag.entity.Role;
 import com.project.oag.entity.User;
 
 @Repository
+@Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long>{
     User findByEmail(String email);
     
@@ -19,6 +23,12 @@ public interface UserRepository extends JpaRepository<User, Long>{
     void delete(User user);
 	/*@Query("SELECT u FROM user u WHERE u.email = ?1")
 	 User findByEmail(String email);*/
+    
+    @Transactional
+    @Modifying
+    @Query("UPDATE users a " +
+            "SET a.enabled = TRUE WHERE a.email = ?1")
+    int enableUser(String email);
 
 	Optional<User> findByUsernameAndPassword(String username, String password);
 
