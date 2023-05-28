876f0e06335c30be91cdaf847cab476858a3a8f6
bereket-7 <bekhonelign@gmail.com>
2023-05-28 12:14:49 -0700

    role enum update

--- src/main/java/com/project/oag/entity/Role.java
+++ src/main/java/com/project/oag/entity/Role.java
@@ -1,50 +1,6 @@
 package com.project.oag.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-
-@Entity
-public class Role {	
-	  	@Id
-	    @GeneratedValue(strategy = GenerationType.AUTO)
-	    private Long id;
-
-		@Enumerated(EnumType.STRING)
-		@Column(length = 20)
-		private RoleType name;
-
-	    public Long getId() {
-			return id;
-		}
-
-		public void setId(Long id) {
-			this.id = id;
-		}
-
-		public RoleType getName() {
-			return name;
-		}
-
-		public void setName(RoleType name) {
-			this.name = name;
-		}
-
-		public Role() {
-	        super();
-	    }
-
-		public Role(String name) {
-			// TODO Auto-generated constructor stub
-		}
-		
-		
-		public enum RoleType {
+public enum Role {
 		    ADMIN,
 		    CUSTOMER,
 		    ARTIST,
@@ -53,4 +9,4 @@ public class Role {
 		    
 		}
 
-}
+
