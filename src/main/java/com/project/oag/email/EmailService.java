a7efb33495be04e908da08b29348feab58f1556a
bereket-7 <bekhonelign@gmail.com>
2023-05-28 13:37:28 -0700

    web sec declaration

--- src/main/java/com/project/oag/email/EmailService.java
+++ src/main/java/com/project/oag/email/EmailService.java
@@ -1,5 +1,38 @@
 package com.project.oag.email;
 
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.MessagingException;
+@Service
+@AllArgsConstructor
 public class EmailService {
+	 private final static Logger LOGGER = LoggerFactory
+	            .getLogger(EmailService.class);
+
+	    private final JavaMailSender mailSender;
+
+	    @Async
+	    public void send(String to, String email) {
+	        try {
+	            MimeMessage mimeMessage = mailSender.createMimeMessage();
+	            MimeMessageHelper helper =
+	                    new MimeMessageHelper(mimeMessage, "utf-8");
+	            helper.setText(email, true);
+	            helper.setTo(to);
+	            helper.setSubject("Confirm your email");
+	            helper.setFrom("kelem.oag.app@gmail.com");
+	            mailSender.send(mimeMessage);
+	        } catch (MessagingException e) {
+	            LOGGER.error("failed to send email", e);
+	            throw new IllegalStateException("failed to send email");
+	        }
+	    }
 
 }
