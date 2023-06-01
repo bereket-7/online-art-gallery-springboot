123ddd3ddeac06958a3667e7c9bc5824aa2fa570
bereket-7 <bekhonelign@gmail.com>
2023-06-01 09:42:50 -0700

    paypal front api update

--- src/main/java/com/project/oag/controller/PaypalController.java
+++ src/main/java/com/project/oag/controller/PaypalController.java
@@ -1,13 +1,7 @@
 package com.project.oag.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
@@ -26,37 +20,69 @@ public class PaypalController {
 		return "home";
 	}
 	@PostMapping("/pay")
-	public String payment(@ModelAttribute("order") Order order) {
+	@ResponseBody
+	public Payment payment(@RequestBody Order order) {
 		try {
 			Payment payment = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
-					order.getIntent(), order.getDescription(), "http://localhost:8080/" + CANCEL_URL,
-					"http://localhost:8080/" + SUCCESS_URL);
-			for (Links link : payment.getLinks()) {
-				if (link.getRel().equals("approval_url")) {
-					return "redirect:" + link.getHref();
-				}
+					order.getIntent(), order.getDescription(), "http://localhost:9090/" + CANCEL_URL,
+					"http://localhost:9090/" + SUCCESS_URL);
+			return payment;
+		} catch (PayPalRESTException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	@GetMapping(value = CANCEL_URL)
+	@ResponseBody
+	public String cancelPay() {
+		return "cancel";
+	}
+	@GetMapping(value = SUCCESS_URL)
+	@ResponseBody
+	public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+		try {
+			Payment payment = service.executePayment(paymentId, payerId);
+			System.out.println(payment.toJSON());
+			if (payment.getState().equals("approved")) {
+				return "success";
 			}
 		} catch (PayPalRESTException e) {
-
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
-		return "redirect:/";
+		return "failure";
 	}
-	 @GetMapping(value = CANCEL_URL)
-	    public String cancelPay() {
-	        return "cancel";
-	    }
-	    @GetMapping(value = SUCCESS_URL)
-	    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
-	        try {
-	            Payment payment = service.executePayment(paymentId, payerId);
-	            System.out.println(payment.toJSON());
-	            if (payment.getState().equals("approved")) {
-	                return "success";
-	            }
-	        } catch (PayPalRESTException e) {
-	         System.out.println(e.getMessage());
-	        }
-	        return "redirect:/";
-	    }
+//	@PostMapping("/pay")
+//	public String payment(@ModelAttribute("order") Order order) {
+//		try {
+//			Payment payment = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
+//					order.getIntent(), order.getDescription(), "http://localhost:8080/" + CANCEL_URL,
+//					"http://localhost:8080/" + SUCCESS_URL);
+//			for (Links link : payment.getLinks()) {
+//				if (link.getRel().equals("approval_url")) {
+//					return "redirect:" + link.getHref();
+//				}
+//			}
+//		} catch (PayPalRESTException e) {
+//
+//			e.printStackTrace();
+//		}
+//		return "redirect:";
+//	}
+//	 @GetMapping(value = CANCEL_URL)
+//	    public String cancelPay() {
+//	        return "cancel";
+//	    }
+//	    @GetMapping(value = SUCCESS_URL)
+//	    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+//	        try {
+//	            Payment payment = service.executePayment(paymentId, payerId);
+//	            System.out.println(payment.toJSON());
+//	            if (payment.getState().equals("approved")) {
+//	                return "success";
+//	            }
+//	        } catch (PayPalRESTException e) {
+//	         System.out.println(e.getMessage());
+//	        }
+//	        return "redirect:/";
+//	    }
 }
--- src/main/java/com/project/oag/service/PaypalService.java
+++ src/main/java/com/project/oag/service/PaypalService.java
@@ -1,34 +1,24 @@
 package com.project.oag.service;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.paypal.api.payments.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.paypal.api.payments.Amount;
-import com.paypal.api.payments.Payer;
-import com.paypal.api.payments.Payment;
-import com.paypal.api.payments.PaymentExecution;
-import com.paypal.api.payments.RedirectUrls;
-import com.paypal.api.payments.Transaction;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.PayPalRESTException;
-
 @Service
 public class PaypalService {
 	@Autowired
 	private APIContext apiContext;
-	public Payment createPayment(
-			Double total, 
-			String currency, 
-			String method,
-			String intent,
-			String description, 
-			String cancelUrl, 
-			String successUrl) throws PayPalRESTException{
+
+	public Map<String, String> createPayment(Double total, String currency, String method, String intent,
+											 String description, String cancelUrl, String successUrl) throws PayPalRESTException {
 		Amount amount = new Amount();
 		amount.setCurrency(currency);
 		total = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
@@ -42,26 +32,82 @@ public class PaypalService {
 		transactions.add(transaction);
 
 		Payer payer = new Payer();
-		payer.setPaymentMethod(method.toString());
+		payer.setPaymentMethod(method);
 
 		Payment payment = new Payment();
-		payment.setIntent(intent.toString());
-		payment.setPayer(payer);  
+		payment.setIntent(intent);
+		payment.setPayer(payer);
 		payment.setTransactions(transactions);
 		RedirectUrls redirectUrls = new RedirectUrls();
 		redirectUrls.setCancelUrl(cancelUrl);
 		redirectUrls.setReturnUrl(successUrl);
 		payment.setRedirectUrls(redirectUrls);
 
-		return payment.create(apiContext);
+		Payment createdPayment = payment.create(apiContext);
+
+		Map<String, String> response = new HashMap<>();
+		for (Links link : createdPayment.getLinks()) {
+			if (link.getRel().equals("approval_url")) {
+				response.put("approvalUrl", link.getHref());
+				response.put("paymentId", createdPayment.getId());
+				break;
+			}
+		}
+
+		return response;
 	}
-	public Payment executePayment(String paymentId, String payerId) throws PayPalRESTException{
+
+	public String executePayment(String paymentId, String payerId) throws PayPalRESTException {
 		Payment payment = new Payment();
 		payment.setId(paymentId);
 		PaymentExecution paymentExecute = new PaymentExecution();
 		paymentExecute.setPayerId(payerId);
-		return payment.execute(apiContext, paymentExecute);
+
+		Payment executedPayment = payment.execute(apiContext, paymentExecute);
+		if (executedPayment.getState().equals("approved")) {
+			return "success";
+		} else {
+			return "failure";
+		}
 	}
 
+//
+//	public Payment createPayment(
+//			Double total,
+//			String currency,
+//			String method,
+//			String intent,
+//			String description,
+//			String cancelUrl,
+//			String successUrl) throws PayPalRESTException{
+//		Amount amount = new Amount();
+//		amount.setCurrency(currency);
+//		total = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
+//		amount.setTotal(String.format("%.2f", total));
+//		Transaction transaction = new Transaction();
+//		transaction.setDescription(description);
+//		transaction.setAmount(amount);
+//		List<Transaction> transactions = new ArrayList<>();
+//		transactions.add(transaction);
+//		Payer payer = new Payer();
+//		payer.setPaymentMethod(method.toString());
+//
+//		Payment payment = new Payment();
+//		payment.setIntent(intent.toString());
+//		payment.setPayer(payer);
+//		payment.setTransactions(transactions);
+//		RedirectUrls redirectUrls = new RedirectUrls();
+//		redirectUrls.setCancelUrl(cancelUrl);
+//		redirectUrls.setReturnUrl(successUrl);
+//		payment.setRedirectUrls(redirectUrls);
+//		return payment.create(apiContext);
+//	}
+//	public Payment executePayment(String paymentId, String payerId) throws PayPalRESTException{
+//		Payment payment = new Payment();
+//		payment.setId(paymentId);
+//		PaymentExecution paymentExecute = new PaymentExecution();
+//		paymentExecute.setPayerId(payerId);
+//		return payment.execute(apiContext, paymentExecute);
+//	}
 }
 
