c6ed72472695cd90b0d582090fec09711bf49b7f
bereket-7 <bekhonelign@gmail.com>
2023-06-02 15:21:12 -0700

    update bid controller

--- src/main/java/com/project/oag/configuration/WebSocketConfig.java
+++ src/main/java/com/project/oag/configuration/WebSocketConfig.java
@@ -4,24 +4,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 import com.project.oag.service.BidHandler;
-@Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
- 
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(bidHandler(), "/bidHandler").setAllowedOrigins("*");
-    }
- 
-    @Bean
-    public WebSocketHandler bidHandler() {
-        return new BidHandler();
-    }
-}
+//@Configuration
+//@EnableWebSocketMessageBroker
+//@EnableWebSocket
+//public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer  {
+
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        registry.enableSimpleBroker("/topic");
+//        registry.setApplicationDestinationPrefixes("/app");
+//    }
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/websocket").withSockJS();
+//    }
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(bidHandler(), "/bidHandler").setAllowedOrigins("*");
+//    }
+//
+//    @Bean
+//    public WebSocketHandler bidHandler() {
+//        return new BidHandler();
+//    }
+//}
     
    /* @Bean
     public SimpMessagingTemplate messagingTemplate() {
