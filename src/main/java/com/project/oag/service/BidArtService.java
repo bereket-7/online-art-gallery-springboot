56dfbc37417a4c1f3234a4c1fc079b02e0d0755f
bereket-7 <bekhonelign@gmail.com>
2023-06-02 12:28:25 -0700

    bidart service and repository

--- src/main/java/com/project/oag/service/BidArtService.java
+++ src/main/java/com/project/oag/service/BidArtService.java
@@ -1,4 +1,20 @@
 package com.project.oag.service;
 
+import com.project.oag.entity.BidArt;
+import com.project.oag.repository.BidArtRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class BidArtService {
+    @Autowired
+    private BidArtRepository bidArtRepository;
+    public Optional<BidArt> getBidArtById(Long id) {
+        return bidArtRepository.findById(id);
+    }
+    public void saveBidArt(BidArt bidArt) {
+        bidArtRepository.save(bidArt);
+    }
 }
