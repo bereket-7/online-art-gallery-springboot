84c2986b3af47a7e47d61086473fdac5977e58d1
bereket honelign <bekhonelign@gmail.com>
2023-06-06 19:37:41 +0300

    resetting repo

--- src/main/java/com/project/oag/repository/CompetitionRepository.java
+++ src/main/java/com/project/oag/repository/CompetitionRepository.java
@@ -1,4 +1,4 @@
-package com.project.oag.repository;
+package repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/project/oag/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/project/oag/repository/PasswordResetTokenRepository.java
@@ -1,4 +1,4 @@
-package com.project.oag.repository;
+package repository;
 
 import java.util.Date;
 import java.util.stream.Stream;
