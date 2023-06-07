ad9f93935e227b5c61c70baae7fa139a5c2b8789
bereket honelign <bekhonelign@gmail.com>
2023-06-07 16:44:06 +0300

    sort by repository

--- src/main/java/com/project/oag/artwork/ArtworkRepository.java
+++ src/main/java/com/project/oag/artwork/ArtworkRepository.java
@@ -14,12 +14,11 @@ import com.project.oag.artwork.Artwork;
 public interface ArtworkRepository  extends JpaRepository<Artwork, Long>{
 	 List<Artwork> findByArtworkCategory(String artworkCategory);
 	 List<Artwork> findByArtistId(int artistId);
-	 List<Artwork> findByPriceBetween(double minPrice, double maxPrice);
+	List<Artwork> findByPriceBetween(int minPrice, int maxPrice);
 	 List<Artwork> findByStatus(String status);
 	  List<Artwork> findAllByOrderByCreateDateDesc();
 	@Query("SELECT a.artworkCategory, COUNT(a) FROM Artwork a GROUP BY a.artworkCategory")
 	List<Object[]> countByCategory();
-
 	@Query("SELECT a FROM Artwork a WHERE a.artworkCategory LIKE %:keyword% " +
 			"OR a.artworkName LIKE %:keyword% OR a.price LIKE %:keyword%")
 	List<Artwork> searchArtwork(@Param("keyword") String keyword, Pageable pageable);
@@ -27,6 +26,8 @@ public interface ArtworkRepository  extends JpaRepository<Artwork, Long>{
 			"OR a.artworkName LIKE %:keyword% OR a.price LIKE %:keyword%")
 	List<String> getAutocompleteResults(@Param("keyword") String keyword);
 
-
+	List<Artwork> findAllByOrderByAverageRatingDesc();
+	List<Artwork> findAllByOrderByPriceAsc();
+	List<Artwork> findAllByOrderByPriceDesc();
 
 }
