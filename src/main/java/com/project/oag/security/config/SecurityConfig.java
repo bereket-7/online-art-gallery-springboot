f48a690efc9ce5a8a80fa2c9a709ec7ba8980a94
bereket honelign <bekhonelign@gmail.com>
2023-06-11 11:21:29 +0300

    security config

---
+++ src/main/java/com/project/oag/security/config/SecurityConfig.java
@@ -0,0 +1,85 @@
+package com.project.oag.security.config;
+
+import com.project.oag.security.filter.JwtTokenFilter;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final JwtTokenFilter jwtAuthenticationFilter;
+    private final UserDetailsService userDetailsService;
+    private final DaoAuthenticationProvider daoAuthenticationProvider;
+
+    public SecurityConfig(JwtTokenFilter jwtAuthenticationFilter,
+                          UserDetailsService userDetailsService,
+                          DaoAuthenticationProvider daoAuthenticationProvider) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.userDetailsService = userDetailsService;
+        this.daoAuthenticationProvider = daoAuthenticationProvider;
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.headers().frameOptions().disable();
+
+        httpSecurity.cors().and().csrf().disable();
+        //@formatter:off
+        httpSecurity
+                .authorizeHttpRequests()
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, authException)
+                                -> response.sendError(
+                                HttpServletResponse.SC_UNAUTHORIZED,
+                                authException.getLocalizedMessage()
+                        )
+                )
+                .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        //@formatter:on
+        return httpSecurity.build();
+    }
+}
--- src/main/java/com/project/oag/security/dto/AuthenticationResponse.java
+++ src/main/java/com/project/oag/security/dto/AuthenticationResponse.java
@@ -1,9 +1,9 @@
 package com.project.oag.security.dto;
 
 import lombok.AllArgsConstructor;
-        import lombok.Getter;
-        import lombok.NoArgsConstructor;
-        import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
