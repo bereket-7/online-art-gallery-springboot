267a146b22586a30a9b3cee345d94cee1428a735
bereket honelign <bekhonelign@gmail.com>
2023-06-11 10:52:30 +0300

    CustomUserDetailsService

---
+++ src/main/java/com/project/oag/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.project.oag.security;
+
+import com.project.oag.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+@Configuration
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            return userRepository.findByUsername(username)
+                    .orElseThrow(() -> new Exception("user Not found "));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
