00b5b2179971a6c4f6e0eb7be6659c58a152711b
bereket honelign <bekhonelign@gmail.com>
2023-06-11 11:26:03 +0300

    Auth controller

---
+++ src/main/java/com/project/oag/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.project.oag.controller;
+import com.project.oag.security.dto.AuthenticationRequest;
+import com.project.oag.security.dto.AuthenticationResponse;
+import com.project.oag.security.utils.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@CrossOrigin
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(
+            @RequestBody AuthenticationRequest authenticationRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                        authenticationRequest.getPassword())
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtTokenProvider.createToken(authentication);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+    @GetMapping("/logout")
+    public ResponseEntity<?> logoutUser() {
+        SecurityContextHolder.clearContext();
+        return ResponseEntity.ok("Logout successful");
+    }
+
+}
