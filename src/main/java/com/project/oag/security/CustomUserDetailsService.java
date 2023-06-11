690729ceeff17e5d460e79240b98c17c4209c010
bereket honelign <bekhonelign@gmail.com>
2023-06-11 10:54:26 +0300

    CustomUserDetailsService fix repo method

--- src/main/java/com/project/oag/security/CustomUserDetailsService.java
+++ src/main/java/com/project/oag/security/CustomUserDetailsService.java
@@ -14,9 +14,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         try {
-            return userRepository.findByUsername(username)
+            return userRepository.findByEmail(email)
                     .orElseThrow(() -> new Exception("user Not found "));
         } catch (Exception e) {
             throw new RuntimeException(e);
