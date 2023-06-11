5f536b019b9181be5b3e4ec1917fdef4c945eecf
bereket honelign <bekhonelign@gmail.com>
2023-06-11 11:09:06 +0300

    AuthenticationResponse

---
+++ src/main/java/com/project/oag/security/dto/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.project.oag.security.dto;
+
+import lombok.AllArgsConstructor;
+        import lombok.Getter;
+        import lombok.NoArgsConstructor;
+        import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String accessToken;
+}
