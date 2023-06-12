652569804d8e7005e7cd139cce97b074fcc475bc
bereket honelign <bekhonelign@gmail.com>
2023-06-12 12:06:34 +0300

    security config filter

--- src/main/java/com/project/oag/security/config/SecurityConfig.java
+++ src/main/java/com/project/oag/security/config/SecurityConfig.java
@@ -1,30 +1,9 @@
 package com.project.oag.security.config;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import org.springframework.context.annotation.Lazy;
-import org.springframework.security.authentication.AuthenticationManager;
-
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import com.project.oag.security.filter.JwtTokenFilter;
-import com.project.oag.service.UserService;
+
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -38,23 +17,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     private final JwtTokenFilter jwtAuthenticationFilter;
-
-    @Qualifier("customUserDetailsService")
     private final UserDetailsService userDetailsService;
-
     private final DaoAuthenticationProvider daoAuthenticationProvider;
 
-    public SecurityConfig(JwtTokenFilter jwtAuthenticationFilter, @Qualifier("customUserDetailsService") UserDetailsService userDetailsService, DaoAuthenticationProvider daoAuthenticationProvider) {
+    public SecurityConfig(JwtTokenFilter jwtAuthenticationFilter,
+                          UserDetailsService userDetailsService,
+                          DaoAuthenticationProvider daoAuthenticationProvider) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
         this.userDetailsService = userDetailsService;
         this.daoAuthenticationProvider = daoAuthenticationProvider;
--- src/main/java/com/project/oag/security/filter/JwtTokenFilter.java
+++ src/main/java/com/project/oag/security/filter/JwtTokenFilter.java
@@ -1,12 +1,11 @@
 package com.project.oag.security.filter;
-
 import com.project.oag.security.utils.JwtTokenProvider;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,17 +15,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
 @Component
+@AllArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
+
     private JwtTokenProvider jwtTokenProvider;
-    @Qualifier("customUserDetailsService")
     private UserDetailsService userDetailsService;
 
-    @Autowired
-    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider, @Qualifier("customUserDetailsService") UserDetailsService userDetailsService) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.userDetailsService = userDetailsService;
-    }
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
