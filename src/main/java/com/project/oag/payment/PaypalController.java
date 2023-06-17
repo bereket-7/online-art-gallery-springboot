8e10663e94d590e74430c8c4456568d23bf49244
bereket honelign <bekhonelign@gmail.com>
2023-06-17 18:24:20 +0300

    paypal controller pay

--- src/main/java/com/project/oag/payment/PaypalController.java
+++ src/main/java/com/project/oag/payment/PaypalController.java
@@ -50,38 +50,4 @@ public class PaypalController {
 		}
 		return "failure";
 	}
-//	@PostMapping("/pay")
-//	public String payment(@ModelAttribute("order") Order order) {
-//		try {
-//			Payment payment = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
-//					order.getIntent(), order.getDescription(), "http://localhost:8080/" + CANCEL_URL,
-//					"http://localhost:8080/" + SUCCESS_URL);
-//			for (Links link : payment.getLinks()) {
-//				if (link.getRel().equals("approval_url")) {
-//					return "redirect:" + link.getHref();
-//				}
-//			}
-//		} catch (PayPalRESTException e) {
-//
-//			e.printStackTrace();
-//		}
-//		return "redirect:";
-//	}
-//	 @GetMapping(value = CANCEL_URL)
-//	    public String cancelPay() {
-//	        return "cancel";
-//	    }
-//	    @GetMapping(value = SUCCESS_URL)
-//	    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
-//	        try {
-//	            Payment payment = service.executePayment(paymentId, payerId);
-//	            System.out.println(payment.toJSON());
-//	            if (payment.getState().equals("approved")) {
-//	                return "success";
-//	            }
-//	        } catch (PayPalRESTException e) {
-//	         System.out.println(e.getMessage());
-//	        }
-//	        return "redirect:/";
-//	    }
 }
