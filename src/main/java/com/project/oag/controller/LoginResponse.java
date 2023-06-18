84c6de29dee95b796c02ea62988d532543c4961c
bereket honelign <bekhonelign@gmail.com>
2023-06-18 21:11:12 +0300

    STADARD MANAGER ROLE

--- src/main/java/com/project/oag/controller/LoginResponse.java
+++ src/main/java/com/project/oag/controller/LoginResponse.java
@@ -1,28 +1,28 @@
-package com.project.oag.controller;
-
-public class LoginResponse {
-    private boolean success;
-    private String message;
-        
-	public LoginResponse() {
-		super();
-	}
-	public LoginResponse(boolean success, String message) {
-		super();
-		this.success = success;
-		this.message = message;
-	}
-	public boolean isSuccess() {
-		return success;
-	}
-	public void setSuccess(boolean success) {
-		this.success = success;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-}
+//package com.project.oag.controller;
+//
+//public class LoginResponse {
+//    private boolean success;
+//    private String message;
+//
+//	public LoginResponse() {
+//		super();
+//	}
+//	public LoginResponse(boolean success, String message) {
+//		super();
+//		this.success = success;
+//		this.message = message;
+//	}
+//	public boolean isSuccess() {
+//		return success;
+//	}
+//	public void setSuccess(boolean success) {
+//		this.success = success;
+//	}
+//	public String getMessage() {
+//		return message;
+//	}
+//	public void setMessage(String message) {
+//		this.message = message;
+//	}
+//
+//}
--- src/main/java/com/project/oag/standard/StandardController.java
+++ src/main/java/com/project/oag/standard/StandardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,11 +25,13 @@ public class StandardController {
 	    }
 
 	    @PostMapping("/add")
+		@PreAuthorize("hasRole('MANAGER')")
 	    public Standard addStandard(@RequestBody Standard standard) {
 	        return standardService.addStandard(standard);
 	    }
 
 	@PutMapping("/{id}")
+	@PreAuthorize("hasRole('MANAGER')")
 	public ResponseEntity<String> updateStandard(@PathVariable Long id, @RequestBody Standard updatedStandard) {
 		Standard standard = standardService.getStandardById(id);
 		if (standard == null) {
@@ -43,6 +46,7 @@ public class StandardController {
 		return ResponseEntity.ok("Standard updated successfully");
 	}
 	    @DeleteMapping("/{id}")
+		@PreAuthorize("hasRole('MANAGER')")
 	    public ResponseEntity<?> deleteStandardById(@PathVariable Long id) {
 	        standardService.deleteStandardById(id);
 	        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
