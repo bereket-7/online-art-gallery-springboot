73c6e7a4804c6f1a0ab5e8bc8344227c9dded0b0
bereket honelign <bekhonelign@gmail.com>
2023-06-22 15:54:14 +0300

    chapa edit

--- src/main/java/com/project/oag/payment/ChapaController.java
+++ src/main/java/com/project/oag/payment/ChapaController.java
@@ -16,9 +16,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
-
+@RestController
+@RequestMapping("/payment")
 public class ChapaController {
     @PostMapping
     public ResponseEntity<PaymentResponse> pay() throws Throwable {
