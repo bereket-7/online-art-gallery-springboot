43f2966fcf7e151963d48b66f67efc28aa1276fe
mekiworld <mekdemalemu5@gmail.com>
2023-06-24 19:08:15 +0300

    sec config update

--- src/main/java/com/project/oag/security/config/SecurityConfig.java
+++ src/main/java/com/project/oag/security/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/artworks/{id}/image").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/artworks/{id}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/artworks").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/artworks/my-artworks").permitAll()
                 .requestMatchers(HttpMethod.DELETE, "/api/artworks/{id}").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/artworks/search").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/artworks/autocomplete").permitAll()
