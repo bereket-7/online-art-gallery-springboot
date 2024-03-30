575fbd009f0b18fee0f676bbc88c746be077e408
bereket-7 <bekhonelign@gmail.com>
2024-03-30 23:45:44 +0300

    GenericResponse update

--- src/main/java/com/project/oag/common/GenericResponse.java
+++ src/main/java/com/project/oag/common/GenericResponse.java
@@ -1,52 +1,12 @@
 package com.project.oag.common;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
+import java.io.Serializable;
 
-public class GenericResponse {
-    private String message;
-    private String error;
-
-    public GenericResponse(final String message) {
-        super();
-        this.message = message;
+    @Builder
+    public record GenericResponse<T>(@JsonProperty("status") int status,
+                                     @JsonProperty("message") String message,
+                                     @JsonProperty("content") T content) implements Serializable {
     }
-
-    public GenericResponse(final String message, final String error) {
-        super();
-        this.message = message;
-        this.error = error;
-    }
-
-    public GenericResponse(List<ObjectError> allErrors, String error) {
-        this.error = error;
-        String temp = allErrors.stream().map(e -> {
-            if (e instanceof FieldError) {
-                return "{\"field\":\"" + ((FieldError) e).getField() + "\",\"defaultMessage\":\"" + e.getDefaultMessage() + "\"}";
-            } else {
-                return "{\"object\":\"" + e.getObjectName() + "\",\"defaultMessage\":\"" + e.getDefaultMessage() + "\"}";
-            }
-        }).collect(Collectors.joining(","));
-        this.message = "[" + temp + "]";
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(final String message) {
-        this.message = message;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(final String error) {
-        this.error = error;
-    }
-
-}
