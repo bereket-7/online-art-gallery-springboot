3f302f4c75403d11a6b400c0c6d20b577f3b7557
bereket-7 <bekhonelign@gmail.com>
2024-04-03 22:42:17 +0300

    RATING model modify

--- src/main/java/com/project/oag/app/model/Rating.java
+++ src/main/java/com/project/oag/app/model/Rating.java
@@ -1,85 +1,45 @@
 package com.project.oag.app.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "RATING")
 public class Rating {
 	 @Id
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	 private Long id;
-	
+
+	@JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
+	@JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "artwork_id")
     private Artwork artwork;
 
-    @Column(nullable = false)
-    private int rating;
-    
-    public Rating() {
-		super();
-	}
+    @Column(name = "RATING_VALUE")
+    private int ratingValue;
 
-	public Rating(User user, Artwork artwork, int rating) {
-		super();
-		this.user = user;
-		this.artwork = artwork;
-		this.rating = rating;
-	}
+	@CreationTimestamp
+	@Column(name = "CREATION_DATE")
+	private Timestamp creationDate;
 
-	//@Column(unique = true, nullable = false)
-    //private String userIdAndArtworkId;
-
-//    public Rating() {
-//        this.userIdAndArtworkId = "";
-//    }
-
-//    public Rating(User user, Artwork artwork, int rating) {
-//        this.user = user;
-//        this.artwork = artwork;
-//        this.rating = rating;
-//        this.userIdAndArtworkId = user.getId() + ":" + artwork.getId();
-//    }
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public Artwork getArtwork() {
-		return artwork;
-	}
-
-	public void setArtwork(Artwork artwork) {
-		this.artwork = artwork;
-	}
-
-	public int getRating() {
-		return rating;
-	}
-
-	public void setRating(int rating) {
-		this.rating = rating;
-	}
+	@UpdateTimestamp
+	@Column(name = "LAST_UPDATE_DATE")
+	private Timestamp lastUpdateDate;
 }
