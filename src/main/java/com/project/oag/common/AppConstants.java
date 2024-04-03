033c1897e64317c9dc293ca907e9940547cd3aaf
bereket-7 <bekhonelign@gmail.com>
2024-04-03 03:23:16 +0300

    app constants

---
+++ src/main/java/com/project/oag/common/AppConstants.java
@@ -0,0 +1,10 @@
+package com.project.oag.common;
+
+public class AppConstants {
+    public static final String EXCEPTION_ON_SENDING_EMAIL = "Exception while sending email.";
+    public static final String TEXT_HTML = "text/html";
+    public static final String NAME_PATTERN = "^[A-Za-z]+(?:\\s[A-Za-z]+)?$";
+    public static final String PHONE_PATTERN = "^(\\+\\d{1,4})?[-.\\s]?\\(?\\d{1,6}\\)?[-.\\s]?\\d{1,10}$";
+    public static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+    public static final String LOG_PREFIX = "Core application {} {}";
+}
