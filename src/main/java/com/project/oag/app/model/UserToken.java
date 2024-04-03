f955b27d93157795863fc11a32f16a57a1a457a1
bereket-7 <bekhonelign@gmail.com>
2024-04-03 20:03:44 +0300

    user token model add

---
+++ src/main/java/com/project/oag/app/model/UserToken.java
@@ -0,0 +1,36 @@
+package com.project.oag.app.model;
+
+import com.project.oag.app.dto.TokenType;
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Setter
+@Getter
+@Builder
+@Table(name = "USER_TOKEN")
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TOKEN_ID", length = 20)
+    private long tokenId;
+
+    @Column(name = "TOKEN", nullable = false)
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "TOKEN_TYPE", nullable = false)
+    private TokenType tokenType;
+
+    @Column(name = "IS_EXPIRED", nullable = false)
+    private boolean expired;
+
+    @Column(name = "IS_REVOKED", nullable = false)
+    private boolean revoked;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
