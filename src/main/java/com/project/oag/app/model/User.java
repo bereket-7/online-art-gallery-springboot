1ef5cbe0f6040ed4850d469d9f5512e98a4b610d
bereket-7 <bekhonelign@gmail.com>
2024-04-03 22:37:40 +0300

    user model modify

--- src/main/java/com/project/oag/app/model/User.java
+++ src/main/java/com/project/oag/app/model/User.java
@@ -1,33 +1,29 @@
 package com.project.oag.app.model;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.project.oag.app.dto.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.jboss.aerogear.security.otp.api.Base32;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
-import lombok.EqualsAndHashCode;
-@EqualsAndHashCode
+import java.sql.Timestamp;
+import java.util.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "USERS")
 public class User implements UserDetails{
     @SequenceGenerator(
             name = "user_sequence",
@@ -35,247 +31,130 @@ public class User implements UserDetails{
             allocationSize = 1
     )
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "user_sequence"
-    )
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "user_sequence")
+	@Column(name = "ID")
     private Long id;
+
+    @Column(name = "FIRSTNAME")
     private String firstname;
+
+    @Column(name = "LASTNAME")
     private String lastname;
-	@Column(unique = true)
+
+    @Column(name = "EMAIL", unique = true)
     private String email;
-	private String phone;
-	private String address;
+
+    @Column(name = "UUID", unique = true)
+    private String uuid = UUID.randomUUID().toString();
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "ADDRESS")
+    private String address;
+
+    @Column(name = "SEX")
     private String sex;
+
+    @Column(name = "AGE")
     private Integer age;
+
+    @Column(name = "USERNAME", unique = true)
     private String username;
+
+    @Column(name = "PASSWORD")
     private String password;
+
+    @Column(name = "LOCKED")
     private Boolean locked = false;
+
+    @Column(name = "ENABLED")
     private Boolean enabled = false;
+
     @Enumerated(EnumType.STRING)
+    @Column(name = "ROLE")
     private Role role;
-	@Lob
-    @Column(name = "Image", nullable = true)
-    private byte[] image;
-	private boolean selectedForBid;
-	private String secret= Base32.random();
-	private boolean isUsing2FA = false;
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Rating> ratings;
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Cart> carts = new ArrayList<>();
+
+    @Column(name = "PHOTO")
+    private String image; //avatar
+
+	@Column(name = "SELECTED_FOR_BID")
+    private boolean selectedForBid;
+
+	@Column(name = "SECRET")
+    private String secret= Base32.random();
+
+	@Column(name = "ENABLED_2FA")
+    private boolean enable2FA = false;
+
+	@JsonIgnoreProperties({"artwork", "user"})
+    @OneToMany(mappedBy = "user")
+    private List<Rating> ratings;
+
 	@JsonIgnore
-	@OneToMany(mappedBy = "artist")
-	private List<Artwork> artworks;
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<Notification> notifications;
+    @OneToMany(mappedBy = "user")
+    private List<Cart> carts = new ArrayList<>();
 
-	public List<Event> getEvents() {
-		return events;
-	}
-	public void setEvents(List<Event> events) {
-		this.events = events;
-	}
-   @JsonIgnore
-	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-	private List<Event> events;
+    @JsonIgnore
+    @OneToMany(mappedBy = "artist")
+    private List<Artwork> artworks;
 
-	public User() {
-	    }
-	public User(String firstname, String lastname, String phone, String username, Integer age, String sex,
-            String address, String email, String password, Role role) {
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.phone = phone;
-    this.username = username;
-    this.age = age;
-    this.sex = sex;
-    this.address = address;
-    this.email = email;
-	//this.password = new BCryptPasswordEncoder().encode(password);
-    this.password = password;
-    this.role = role;
-}
-	public List<Cart> listCartItems() {
-		return carts;
-	}
-	public List<Cart> getCarts() {
-		return carts;
-	}
+	@JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Notification> notifications;
 
-	public void setCarts(List<Cart> carts) {
-		this.carts = carts;
-	}
-	public void addCart(Cart cart) {
-		carts.add(cart);
-		cart.setUser(this);
-	}
-	public void removeCart(Cart cart) {
-		carts.remove(cart);
-		cart.setUser(null);
-	}
-	public void clearCarts() {
-		carts.clear();
-	}
+	@JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Event> events;
 
-	public List<Notification> getNotifications() {
-		return notifications;
-	}
+    @CreationTimestamp
+    @Column(name = "CREATION_DATE")
+    private Timestamp creationDate;
 
-	public void setNotifications(List<Notification> notifications) {
-		this.notifications = notifications;
-	}
+    @UpdateTimestamp
+    @Column(name = "LAST_UPDATE_DATE")
+    private Timestamp lastUpdateDate;
 
-	public Long getId() {
-		return id;
-	}
+    public void addCart(Cart cart) {
+        carts.add(cart);
+        cart.setUser(this);
+    }
+    public void removeCart(Cart cart) {
+        carts.remove(cart);
+        cart.setUser(null);
+    }
+    public void clearCarts() {
+        carts.clear();
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-	public Boolean getLocked() {
-		return locked;
-	}
-	public void setLocked(Boolean locked) {
-		this.locked = locked;
-	}
-	public Boolean getEnabled() {
-		return enabled;
-	}
-	public void setEnabled(Boolean enabled) {
-		this.enabled = enabled;
-	}
-	public Role getRole() {
-		return role;
-	}
-	public void setRole(Role role) {
-		this.role = role;
-	}
-	public byte[] getImage() {
-		return image;
-	}
-	public void setImage(byte[] image) {
-		this.image = image;
-	}
-	public String getSecret() {
-		return secret;
-	}
-	public void setSecret(String secret) {
-		this.secret = secret;
-	}
-	public boolean isUsing2FA() {
-		return isUsing2FA;
-	}
-	public void setUsing2FA(boolean isUsing2FA) {
-		this.isUsing2FA = isUsing2FA;
-	}
-	public List<Rating> getRatings() {
-		return ratings;
-	}
-	public void setRatings(List<Rating> ratings) {
-		this.ratings = ratings;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public void setPassword(String password){
-		  this.password = password;
-	}
-	public boolean getSelectedForBid() {
-		  return selectedForBid;
-	}
-	public List<Artwork> getArtworks() {
-		return artworks;
-	}
-
-	public void setArtworks(List<Artwork> artworks) {
-		this.artworks = artworks;
-	}
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		SimpleGrantedAuthority authority =
-				new SimpleGrantedAuthority(role.name());
-		return Collections.singletonList(authority);
-	}
-	@Override
-	public String getPassword() {
-		return password;
-	}
-	@Override
-	public String getUsername() {
-		return email;
-	}
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-	@Override
-	public boolean isAccountNonLocked() {
-		return !locked;
-	}
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-	@Override
-	public boolean isEnabled() {
-		return enabled;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(role.name());
+        return Collections.singletonList(authority);
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
