9e2193791218fef7124b2f3bfc13926ba9a57d8a
bereket-7 <bekhonelign@gmail.com>
2024-04-03 20:41:16 +0300

    Competitor model update

--- src/main/java/com/project/oag/app/model/Competitor.java
+++ src/main/java/com/project/oag/app/model/Competitor.java
@@ -2,152 +2,70 @@ package com.project.oag.app.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "competitor")
+@Table(name = "COMPETITOR")
 public class Competitor {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	private String firstName;
-	private String lastName;
-	private String email;
-	private String phone;
-	@Lob
-    @Column(name = "Image", length = Integer.MAX_VALUE, nullable = true)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ID;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Lob
+    @Column(name = "IMAGE")
     private byte[] image;
-	private String artDescription;
-	private String category;
-	private int vote;
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "competition_id", nullable = true)
-	@JsonIgnore
-	private Competition competition;
-	@OneToMany(mappedBy = "competitor", cascade = CascadeType.ALL)
-	private List<Vote> votes;
 
-	public Competitor() {
-		super();
-	}
-	public Competitor(String firstName, String lastName, String email, String phone, byte[] image,
-			String artDescription, String category, int vote, Competition competition) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.phone = phone;
-		this.image = image;
-		this.artDescription = artDescription;
-		this.category = category;
-		this.vote = vote;
-		this.competition = competition;
-	}
+    @Column(name = "ARTWORK_DESCRIPTION")
+    private String artworkDescription;
 
-	public void addVote(Vote vote) {
-		votes.add(vote);
-		vote.setCompetitor(this);
-	}
+    @Column(name = "CATEGORY")
+    private String category;
 
-	public void removeVote(Vote vote) {
-		votes.remove(vote);
-		vote.setCompetitor(null);
-	}
+    @Column(name = "VOTE")
+    private int vote;
 
-	public void incrementVoteCount() {
-		vote++;
-	}
-	public Competitor(String filename, String string) {
-	}
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COMPETITION_ID")
+    @JsonIgnore
+    private Competition competition;
 
-	public List<Vote> getVotes() {
-		return votes;
-	}
+    @OneToMany(mappedBy = "competitor", cascade = CascadeType.ALL)
+    private List<Vote> votes;
 
-	public void setVotes(List<Vote> votes) {
-		this.votes = votes;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getArtDescription() {
-		return artDescription;
-	}
-
-	public void setArtDescription(String artDescription) {
-		this.artDescription = artDescription;
-	}
-
-	public Competition getCompetition() {
-		return competition;
-	}
-
-	public void setCompetition(Competition competition) {
-		this.competition = competition;
-	}
-
-	public byte[] getImage() {
-		return image;
-	}
-
-	public void setImage(byte[] image) {
-		this.image = image;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public int getVote() {
-		return vote;
-	}
-
-	public void setVote(int vote) {
-		this.vote = vote;
-	}
-	
-
+    //@JsonIgnoreProperties({"notifications","artworks"})
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+    public void addVote(Vote vote) {
+        votes.add(vote);
+        vote.setCompetitor(this);
+    }
+    public void removeVote(Vote vote) {
+        votes.remove(vote);
+        vote.setCompetitor(null);
+    }
+    public void incrementVoteCount() {
+        vote++;
+    }
 }
