3cd4b3e8ffc134d720814156a9dd4461a00ad535
bereket-7 <bekhonelign@gmail.com>
2024-04-05 01:20:08 +0300

    UserRequestDto

---
+++ src/main/java/com/project/oag/app/dto/UserRequestDto.java
@@ -0,0 +1,19 @@
+package com.project.oag.app.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class UserRequestDto {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String address;
+    private String sex;
+    private Integer age;
+    private String username;
+    private String password;
+	private Role role;
+}
