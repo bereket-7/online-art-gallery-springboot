b4f5bd8037633328198b12e7b523120bd748e546
bereket-7 <bekhonelign@gmail.com>
2024-04-04 04:41:38 +0300

    docker initial

--- src/main/java/com/project/oag/config/SwaggerConfig.java
+++ src/main/java/com/project/oag/config/SwaggerConfig.java
@@ -11,8 +11,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
-@EnableWebMvc
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
+
         @Bean
         public Docket api() {
             return new Docket(DocumentationType.SWAGGER_2).select()
@@ -20,7 +20,6 @@ public class SwaggerConfig implements WebMvcConfigurer {
                     .paths(PathSelectors.regex("/.*"))
                     .build().apiInfo(apiInfoMetaData());
         }
-
     private ApiInfo apiInfoMetaData() {
             return new ApiInfoBuilder().title("OAG")
                     .description("Online Art Gallery")
