03e18dc751ff83859834a4a228043b4ca2f937d4
bereket-7 <bekhonelign@gmail.com>
2024-04-06 00:11:11 +0300

    json util

---
+++ src/main/java/com/project/oag/utils/JsonUtils.java
@@ -0,0 +1,27 @@
+package com.project.oag.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.oag.exceptions.GeneralException;
+
+
+public class JsonUtils {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new GeneralException("Error converting object to JSON");
+        }
+    }
+
+    public static <T> T fromJson(String json, TypeReference<T> typeReference) {
+        try {
+            return objectMapper.readValue(json, typeReference);
+        } catch (JsonProcessingException e) {
+            throw new GeneralException("Error converting JSON to object");
+        }
+    }
+}
