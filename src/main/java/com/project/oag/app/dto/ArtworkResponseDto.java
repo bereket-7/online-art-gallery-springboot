27c0ed5e05cf46c75ee578ba49c0492d695c8c46
bereket-7 <bekhonelign@gmail.com>
2024-04-06 21:52:11 +0300

    artwork response dto

---
+++ src/main/java/com/project/oag/app/dto/ArtworkResponseDto.java
@@ -0,0 +1,17 @@
+package com.project.oag.app.dto;
+
+import com.project.oag.app.model.Rating;
+import lombok.Data;
+
+import java.util.List;
+@Data
+public class ArtworkResponseDto {
+    private String artworkName;
+    private String artworkDescription;
+    private String artworkCategory;
+    private int price;
+    private String size;
+    private List<String> imageUrls;
+    private List<Rating> ratings;
+    private Long artistId;
+}
