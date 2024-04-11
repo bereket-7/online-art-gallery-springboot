5f282ce135d76170d2194ce98cbfd5369b3bf917
bereket-7 <bekhonelign@gmail.com>
2024-04-11 23:54:43 +0300

    get carts controller

--- src/main/java/com/project/oag/app/controller/CartController.java
+++ src/main/java/com/project/oag/app/controller/CartController.java
@@ -1,11 +1,10 @@
 package com.project.oag.app.controller;
 
-import com.project.oag.app.dto.CartDTO;
+import com.project.oag.app.dto.CartDto;
 import com.project.oag.app.service.ArtworkService;
 import com.project.oag.app.service.CartService;
 import com.project.oag.common.GenericResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -31,38 +30,10 @@ public class CartController {
         return cartService.addToCart(request,artworkId, quantity);
     }
 
-//    @GetMapping
-//    public ResponseEntity<List<CartDTO>> getCarts(@AuthenticationPrincipal UserDetails userDetails) {
-//        String email = userDetails.getUsername();
-//        List<Cart> carts = cartService.getCartsByEmail(email);
-//
-//        List<CartDTO> cartDTOs = carts.stream()
-//                .map(cart -> new CartDTO(cart.getId(), cart.getArtwork().getArtworkName(), cart.getQuantity()))
-//                .collect(Collectors.toList());
-//        return ResponseEntity.ok(cartDTOs);
-//    }
-//    @GetMapping
-//    public ResponseEntity<List<CartDTO>> getCarts(@AuthenticationPrincipal UserDetails userDetails) {
-//        String email = userDetails.getUsername();
-//        List<Cart> carts = cartService.getCartsByEmail(email);
-//        System.out.println("Cart size: " + carts.size());
-//
-//        List<CartDTO> cartDTOs = new ArrayList<>();
-////                .map(cart -> new CartDTO(
-////                        cart.getId(),
-////                        cart.getArtwork().getArtworkName(), // Populate the artwork details
-////                        cart.getQuantity()
-////                ))
-////                .collect(Collectors.toList());
-//        return ResponseEntity.ok(cartDTOs);
-//    }
-
-
     @GetMapping
-    public ResponseEntity<List<CartDTO>> getCartsByEmail(@AuthenticationPrincipal UserDetails userDetails) {
-        String email = userDetails.getUsername();
-        List<CartDTO> cartDTOs = cartService.getCartsByEmail(email);
-        return ResponseEntity.ok(cartDTOs);
+    @PreAuthorize("hasAuthority('USER_MODIFY_CART')")
+    public ResponseEntity<GenericResponse> getCartsByEmail(HttpServletRequest request) {
+       return cartService.getCarts(request);
     }
     @DeleteMapping("/remove/{cartId}")
     public ResponseEntity<String> removeFromCart(@AuthenticationPrincipal UserDetails userDetails, @PathVariable Long cartId) {
