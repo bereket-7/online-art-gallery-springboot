84f418039a113a5b2139eabc319b041f30428998
bereket-7 <bekhonelign@gmail.com>
2024-06-08 03:04:37 +0300

    ChapaConfig

---
+++ src/main/java/com/project/oag/config/properties/ChapaConfig.java
@@ -0,0 +1,22 @@
+package com.project.oag.config.properties;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationPropertiesScan
+@ConfigurationProperties(prefix = "chapa")
+public class ChapaConfig {
+    @NotNull
+    String apiKey;
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+}
