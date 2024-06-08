f176b8b89d8e6c48f7db8c882404fd3e5451df9f
bereket-7 <bekhonelign@gmail.com>
2024-06-09 01:43:50 +0300

    UserRepository update

--- src/main/java/com/project/oag/app/repository/UserRepository.java
+++ src/main/java/com/project/oag/app/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.project.oag.app.repository;
 
 import com.project.oag.app.dto.Role;
 import com.project.oag.app.entity.User;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,11 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+import static com.project.oag.common.AppConstants.CACHE_KEY_EMAIL;
+import static com.project.oag.common.AppConstants.CACHE_NAME_USER_INFO;
+
 @Repository
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailIgnoreCase(String email);
-
+    boolean existsByEmailIgnoreCase(String email);
     List<User> findByUsernameContainingIgnoreCase(String username);
 
     Long countTotalUsersByRole(Role role);
@@ -30,5 +34,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
     int enableUser(String email);
 
     List<User> findByRole(Role role);
-
+    @Transactional
+    @Modifying
+    @Query("update User u set u.password = ?1 where upper(u.email) = upper(?2)")
+    @CacheEvict(value = CACHE_NAME_USER_INFO, key = CACHE_KEY_EMAIL)
+    int updatePasswordByEmailIgnoreCase(String password, String email);
+    @Transactional
+    @Modifying
+    @Query("update User u set u.verified = ?1 where upper(u.email) = upper(?2)")
+    @CacheEvict(value = CACHE_NAME_USER_INFO, key = CACHE_KEY_EMAIL)
+    int updateVerifiedByEmailIgnoreCase(boolean verified, String email);
+    @Query("select (count(u) > 0) from User u where upper(u.email) = upper(?1) and u.userRole.admin = ?2")
+    boolean existsByUsernameAndIsAdmin(String email, boolean admin);
 }
