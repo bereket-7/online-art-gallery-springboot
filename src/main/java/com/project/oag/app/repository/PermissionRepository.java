3ea55cb004e09986cd13adbd20a5414206d516f3
bereket-7 <bekhonelign@gmail.com>
2024-06-11 03:48:35 +0300

    RoleGenericResponse

--- src/main/java/com/project/oag/app/repository/PermissionRepository.java
+++ src/main/java/com/project/oag/app/repository/PermissionRepository.java
@@ -4,6 +4,12 @@ import com.project.oag.app.entity.RolePermission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface PermissionRepository extends JpaRepository<RolePermission, Long> {
+    List<RolePermission> findByAssignableTrueAndAdmin(boolean admin);
+
+    List<RolePermission> findByPermissionIdInAndAssignableTrueAndAdmin(Collection<Long> permissionIds, boolean admin);
 }
