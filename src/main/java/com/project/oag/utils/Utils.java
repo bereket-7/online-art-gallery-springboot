f6fb319d0a3a4aaa3cb503f42cb77ee5d5221a9a
bereket-7 <bekhonelign@gmail.com>
2024-06-25 22:17:04 +0300

    Utils update

--- src/main/java/com/project/oag/utils/Utils.java
+++ src/main/java/com/project/oag/utils/Utils.java
@@ -1,12 +1,23 @@
 package com.project.oag.utils;
 
+import com.project.oag.app.dto.GenericResponsePageable;
+import com.project.oag.app.dto.PageableDto;
 import com.project.oag.common.GenericResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
 
 public class Utils {
     public static ResponseEntity<GenericResponse> prepareResponse(final HttpStatus status, final String message, final Object content) {
         return ResponseEntity.status(status)
                 .body(new GenericResponse(status.value(), message, content));
+    }
+    public static ResponseEntity<GenericResponsePageable> prepareResponseWithPageable(final HttpStatus status, final String message, final Object content, PageableDto pageableDto) {
+        return ResponseEntity.status(status)
+                .body(new GenericResponsePageable<>(status.value(), message, content, pageableDto));
+    }
+    public static String getRandomUuid() {
+        return UUID.randomUUID().toString();
     }
 }
