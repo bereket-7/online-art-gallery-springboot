be1ad674685fea95aba9a33ce5590e5641891dac
bereket-7 <bekhonelign@gmail.com>
2024-10-02 03:43:04 +0300

    XSSRequestWrapper update

--- src/main/java/com/project/oag/config/security/XSSRequestWrapper.java
+++ src/main/java/com/project/oag/config/security/XSSRequestWrapper.java
@@ -1,13 +1,16 @@
 package com.project.oag.config.security;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.project.oag.exceptions.XSSServletException;
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.owasp.encoder.Encode;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.List;
 
 public class XSSRequestWrapper extends HttpServletRequestWrapper {
@@ -55,6 +58,68 @@ public class XSSRequestWrapper extends HttpServletRequestWrapper {
             }
         }
         body = stringBuilder.toString();
+    }
+
+    private boolean sanitize(String input) {
+        if (!XSSValidationUtils.isValidURL(input, skipWords)) {
+            ErrorResponse errorResponse = new ErrorResponse(HttpStatus.FORBIDDEN.value(), "XSS attack error");
+            try {
+                String response = XSSValidationUtils.convertObjectToJson(errorResponse);
+                throw new XSSServletException(response);
+            } catch (JsonProcessingException e) {
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
+    public String getParameter(String paramName) {
+        String value = super.getParameter(paramName);
+        sanitize(value);
+        return Encode.forHtml(value);
+    }
+
+    @Override
+    public String[] getParameterValues(String paramName) {
+        String[] values = super.getParameterValues(paramName);
+        if (values != null) {
+            for (int index = 0; index < values.length; index++) {
+                sanitize(values[index]);
+                values[index] = Encode.forHtml(values[index]);
+            }
+        }
+        return values;
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body.getBytes());
+        return new ServletInputStream() {
+            @Override
+            public boolean isFinished() {
+                return byteArrayInputStream.available() == 0;
+            }
+
+            @Override
+            public boolean isReady() {
+                return true;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener) {
+                // No implementation needed
+            }
+
+            @Override
+            public int read() throws IOException {
+                return byteArrayInputStream.read();
+            }
+        };
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(this.getInputStream()));
     }
 
 }
