765fbf209fe0508ecc95acf2c8be8b967ae8bf75
bereket-7 <bekhonelign@gmail.com>
2025-05-13 23:23:55 +0300

    enhance DataSeederService to include user role assignment and additional user attributes

--- src/main/java/com/project/oag/utils/DataSeederService.java
+++ src/main/java/com/project/oag/utils/DataSeederService.java
@@ -3,9 +3,12 @@ package com.project.oag.utils;
 import com.project.oag.app.dto.StandardType;
 import com.project.oag.app.entity.Standard;
 import com.project.oag.app.entity.User;
+import com.project.oag.app.entity.UserRole;
+import com.project.oag.app.repository.RoleRepository;
 import com.project.oag.app.repository.StandardRepository;
 import com.project.oag.app.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
+import lombok.val;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +22,13 @@ public class DataSeederService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public DataSeederService(StandardRepository standardRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    private final RoleRepository userRoleRepository;
+
+    public DataSeederService(StandardRepository standardRepository, UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository userRoleRepository) {
         this.standardRepository = standardRepository;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.userRoleRepository = userRoleRepository;
     }
 
     @PostConstruct
@@ -52,13 +58,24 @@ public class DataSeederService {
                 user.setFirstName("TestFirstName" + i);
                 user.setLastName("TestLastName" + i);
                 user.setEmail("testuser" + i + "@example.com");
-                user.setUsername("testuser" + i);
                 user.setPassword(passwordEncoder.encode("password" + i));
-                user.setEnabled(true);
-                user.setVerified(true);
+                user.setPhone("123456789" + i);
+                user.setAddress("Test Address " + i);
+                user.setSex(i % 2 == 0 ? "Male" : "Female");
+                user.setAge(20 + (i % 10));
+                user.setImage("avatar" + i + ".png");
+                user.setSelectedForBid(false);
+                user.setResetPasswordToken(null);
+                user.setUserRole(getDefaultUserRole());
                 users.add(user);
             }
             userRepository.saveAll(users);
         }
+    }
+
+    private UserRole getDefaultUserRole() {
+        val userRole = userRoleRepository.findByRoleNameIgnoreCase("ROLE_CUSTOMER").
+                orElse(null);
+        return userRole;
     }
 }
